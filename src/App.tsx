2368c30b941953cc146ed5853384aca6020fcfcc
MakandarSahil <sahilmakandar15@gmail.com>
2025-03-30 01:36:43 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen w-full text-white flex items-center justify-center'>Hii</div>
+  )
+}
+
+export default App
